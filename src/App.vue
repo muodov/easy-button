b09d891b66b33ae8b4b774632cc6ca0a89647d6f
Maxim Tsoy <muodov@gmail.com>
2017-01-06 15:20:15 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <span class="button-wrap"><a href="#" class="easy-button button button-circle button-caution" @click="play"></a></span>
+  </div>
+</template>
+
+<script>
+let soundUrl = require("file-loader!./easy.mp3");
+export default {
+  name: 'app',
+  methods: {
+    play () {
+      new Audio(soundUrl).play()
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "buttons.css";
+.easy-button {
+  width: 300px;
+  height: 300px;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
